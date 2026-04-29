5945630b6b302530a056173b25b522f1b0c9b647
Meghana Poojary <meghanapoojary121@gmail.com>
2026-04-29 11:06:18 +0530

    Final Commit

--- server/app.py
+++ server/app.py
@@ -134,6 +134,6 @@ def predict():
         if temp_path and os.path.exists(temp_path):
             os.remove(temp_path)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
